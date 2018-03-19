5508d7f078f4021c310102b4ebf6a11de89140da
jay5585 <jaytrivedi5585@gmail.com>
2018-03-19 23:21:55 +0530

    first_commit

---
+++ Student_GPA/OtherClasses/Webservice/Utility.swift
@@ -0,0 +1,13 @@
+//
+//  Utility.swift
+//  Student_GPA
+//
+//  Created by VIJAY on 14/03/18.
+//  Copyright © 2018 TestDemo. All rights reserved.
+//
+
+import UIKit
+
+class Utility: NSObject {
+
+}
---
+++ Student_GPA/ViewController/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  Student_GPA
+//
+//  Created by VIJAY on 14/03/18.
+//  Copyright © 2018 TestDemo. All rights reserved.
+//
+
+import UIKit
+
+class Constant: NSObject {
+
+}
